9b1100c9f4424a3e7ddfa191e84518630c9dc14d
Qasim Nouh <vs.dev50@gmail.com>
2022-08-28 17:23:05 +0300

    [ADD] nginx access log to access.log only [ADD] nano + normal vim

--- image/utilities.sh
+++ image/utilities.sh
@@ -4,8 +4,7 @@ source /bd_build/buildconfig
 set -x
 
 ## Often used tools.
-$minimal_apt_get_install curl git less vim-tiny psmisc gpg-agent dirmngr
-ln -s /usr/bin/vim.tiny /usr/bin/vim
+$minimal_apt_get_install curl git less vim nano psmisc gpg-agent dirmngr
 
 ## This tool runs a command as another user and sets $HOME.
 cp /bd_build/bin/setuser /sbin/setuser
@@ -15,3 +14,6 @@ cp /bd_build/bin/install_clean /sbin/install_clean
 
 ## Copy php opcache config to php conf.d directory
 cp /bd_build/php/opcache.ini /usr/local/etc/php/conf.d/opcache.ini
+
+# Nginx access log on /var/log/access.log only
+sed -i.bak 's/access.log = \/proc\/self\/fd\/2/access.log = \/proc\/self\/fd\/1/g' /usr/local/etc/php-fpm.d/docker.conf
